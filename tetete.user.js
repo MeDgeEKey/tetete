e438509c8529a3d70f474546906c5607afb2100f
MeDgeEKey <51380260+MeDgeEKey@users.noreply.github.com>
2024-07-04 17:55:24 +0300

    Update tetete.user.js

--- tetete.user.js
+++ tetete.user.js
@@ -107,7 +107,7 @@ try {
 
     function resetGameSettings() {
         GAME_SETTINGS = {
-            minBombHits: Math.floor(Math.random() * 2),
+            minBombHits: Math.floor(Math.random() * 50),
             minIceHits: Math.floor(Math.random() * 2) + 2,
             flowerSkipPercentage: Math.floor(Math.random() * 25) + 18,
             minDelayMs: 2000,
