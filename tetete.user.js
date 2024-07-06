2a021c96f59127804dbaae1be4e53899800f0f10
MeDgeEKey <51380260+MeDgeEKey@users.noreply.github.com>
2024-07-06 05:18:38 +0300

    Update tetete.user.js

--- tetete.user.js
+++ tetete.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Blume miner
-// @version      1.23
+// @version      1.24
 // @namespace    cheltbl
 // @author       cheltbl
 // @match        https://telegram.blum.codes/*
